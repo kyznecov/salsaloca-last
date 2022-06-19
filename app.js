e3401d887393a0818dfdb88108f191c24004c392
kyznecov <58451637+kyznecov@users.noreply.github.com>
2022-06-19 22:25:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+VANTA.FOG({
+	el: "#intro",
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	highlightColor: 0x0,
+	midtoneColor: 0x1e2269,
+	lowlightColor: 0x1b1b52,
+	baseColor: 0x0,
+	blurFactor: 0.32,
+	speed: 0.6,
+	zoom: 1.70
+})
